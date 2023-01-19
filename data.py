e4ca5ea3ab1c96decd8ab0dd37169df1f619d755
wellinton <wellinton1067@gmail.com>
2023-01-19 17:05:02 -0300

    add text.py

--- data.py
+++ data.py
@@ -22,8 +22,48 @@ list2 = [1,3,5,7,9]
 
 def merger_sort_list(li: list, li2: list):
     li.extend(li2)
-    return sorted(li)
+    return list.sort(li)
 
 print(merger_sort_list(list1, list2))
 
+
+print(list1.pop())
+print(list2)
         
+
+dict1 = {
+    'age': 54,
+    'name': 'Wellinton'
+}
+
+
+list10 = [2,5,1,2,3,5,1,2,4]
+list20 = [2,1,1,2,3,5,1,2,4]
+list30 = [2,3,4,5]
+
+def first_recurring_character2(li: list):
+    n = 1
+    for i in range(len(li)):
+        for j in range(n, len(li)):
+            if li[i] == li[j]:
+                return li[i]
+        n += 1
+
+def first_recurring_character(li: list):
+    dict = {}
+    for i in li:
+        if not dict.get(i):
+            dict.update({i: True})
+        else:
+            return i
+
+
+                
+
+print(first_recurring_character(list10))
+print(first_recurring_character(list20))
+print(first_recurring_character(list30))
+
+print(first_recurring_character2(list10))
+print(first_recurring_character2(list20))
+print(first_recurring_character2(list30))
---
+++ text.py
@@ -0,0 +1,30 @@
+
+def reverse_string(text: str):
+    letter_list = [word for word in text]
+    return ''.join(reversed(letter_list))
+    
+
+text1 = "This code uses a defaultdict(int), which is a subclass of the built-in dict class in Python that returns a default value of 0 for nonexistent keys. This allows us to increment the count for a word without first having to check if it's already in the hash table. The code takes a string of text, split it into list of words and count the occurrences of each word by keeping track of the count in the hash table and finally it prints the result."
+
+print(reverse_string(text1))
+
+def letter_count2(text: str):
+    word_list = []
+    for i in text:
+        if i != ' ':
+            word_list.append(i)
+    return word_list
+
+print(letter_count2(text1))
+
+def word_count(text: str):
+    words = 0
+    word_list = text.split()
+    for i in word_list:
+        words += 1
+    return word_list
+
+print(word_count(text1))
+
+
+    
