99c234a71460fd2ea1d7b60118039ee8ab6e91b7
wellinton <wellinton1067@gmail.com>
2023-01-20 16:10:57 -0300

    add insert and remove linde2.py

--- linked2.py
+++ linked2.py
@@ -21,27 +21,59 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
         self.length += 1
-    
-    def print_list(self):
+
+    def transverse(self, index):
+        i=0
         current_node = self.head
-        while current_node:
-            print(current_node.value)
+        while i < index:
             current_node = current_node.next
+            i+=1
+        return current_node
+
+    def insert(self, value, index):
+        if index == 0:
+            self.prepend(value)
+            return
+        if index >= self.length:
+            self.append(value)
+            return
+        leader = self.transverse(index-1)
+        pointer = self.transverse(index)
+        new_node = Node(value)
+        leader.next = new_node
+        new_node.next = pointer
+        self.length +=1
+    
+    def remove(self, index):
+        if index == 0:
+            self.head = self.head.next
+            self.length -= 1
+            return
+        leader = self.transverse(index-1)
+        pointer = self.transverse(index+1)
+        leader.next = pointer
+        self.length -=1
+
+    def print_list(self):
+        list_linked = []
+        current_node = self.head
+        while current_node != None:
+            list_linked.append(current_node.value)
+            current_node = current_node.next
+        return print(list_linked)
     
     def print_tail(self):
-        current_tail = self.tail.value
+        current_tail = self.tail.next
         print(current_tail)
 
 myLink = LinkedList(10)
 myLink.append(25)
 myLink.append(52)
 myLink.prepend(15)
-# myLink.print_list()
+myLink.prepend(14)
+myLink.prepend(13)
 
-list2 = [1,2,3,5,6,879,36]
 
-for i in range(len(list2)):
-    myLink.prepend(list2[i])
-
-# myLink.print_list()
-myLink.print_tail()
+myLink.print_list()
+# print(myLink.length)
+# myLink.print_tail()
