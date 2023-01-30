c8cc95ce7e8a4183a8cda9dcfbd104742d2b418f
wellinton <wellinton1067@gmail.com>
2023-01-30 07:11:30 -0300

    add Palindrome recursion

---
+++ palindrome_recursion.py
@@ -0,0 +1,11 @@
+def isPalindrome(str):
+    if len(str) == 0 or len(str) == 1:
+        return True
+    
+    if str[0] == str[len(str)-1]:
+        return isPalindrome(str[1:len(str)-1])
+
+    return False
+
+
+print(isPalindrome('kayyak'))
